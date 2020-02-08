9a888363ce4a66f9aaaf4c8ff8c422439396f83c
lilcappucc <lilcappucc@gmail.com>
2020-02-08 17:59:51 +0200

    Initial Commit

---
+++ KvartaKavy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KvartaKavy
+//
+//  Created by dewill on 08.02.2020.
+//  Copyright © 2020 lilcappucc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
